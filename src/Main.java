6c8b27f07d47705ff107def72ddd014c2115ebb5
Magnus F <sabbathmagnus@gmail.com>
2024-09-11 14:46:19 +0200

    More progress

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,79 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+        int scoreCount = 0;
+        ArrayList<Integer> scoresList = new ArrayList<Integer>();
+        printMenu(scoreCount);
         String userInput = scanner.nextLine();
-        ArrayList<String> scoresList = new ArrayList<String>();
-        printMenu();
-        switch (userInput) {
-            case "1":
-            case "2":
-            case "3":
-            case "4":
-            case "5":
+        while (running) {
+            if (checkInput(userInput)) {
+                switch (userInput) {
+                    case "1": {
+                        addScore();
+                    }
+                    case "2": {
+                        printHighscore(scoresList);
+                    }
+                    case "3": {
+                        printAverageScore(scoresList);
+                    }
+                    case "4": {
+                        clearScoreList(scoresList);
+                        System.out.println("List cleared...");
+                        printMenu(scoreCount);
+                        scanner.nextLine();
+                        break;
+                    }
+                    case "5": {
+                        System.out.println("Exiting...");
+                        running = false;
+
+                    }
+                }
+            } else {
+                System.out.println("Incorrect input");
+                printMenu(scoreCount);
+                scanner.nextLine();
+            }
         }
     }
-    private static void printMenu() {
+
+    private static void clearScoreList(ArrayList<Integer> scoresList) {
+        scoresList.clear();
+    }
+
+    private static void printAverageScore(ArrayList<Integer> scoresList) {
+        int totalScore = 0;
+        for (int score : scoresList) {
+            totalScore = totalScore + score;
+        }
+        double averageScore = (double) totalScore /scoresList.size();
+        System.out.println("The average score of " +scoresList.size() +" scores is: " +averageScore);
+    }
+
+    private static void printHighscore(ArrayList<Integer> scoresList) {
+
+    }
+
+    private static void addScore() {
+    }
+
+    private static void printMenu(int scoreCount) {
+        System.out.println("Total number of scores in list: " + scoreCount);
         System.out.println("1. Add score");
         System.out.println("2. Show highest score");
         System.out.println("3. Show average score");
         System.out.println("4. Clear list");
         System.out.println("5. Exit");
+        System.out.println("Enter:");
+    }
+
+    private static boolean checkInput(String input) {
+        return input.equals("1") ||
+                input.equals("2") ||
+                input.equals("3") ||
+                input.equals("4") ||
+                input.equals("5");
     }
 }
